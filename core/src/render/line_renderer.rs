55577582cc2c1eb0578060fb9d7dfb6bbdb2ea00
CoffeeCatRailway <dragonlee1002@gmail.com>
2026-01-11 11:42:31 +1100

    WASM working

--- core/src/render/line_renderer.rs
+++ core/src/render/line_renderer.rs
@@ -2,7 +2,7 @@
 
 use std::rc::Rc;
 use glam::{Mat4, Vec3};
-use glow::{Buffer, HasContext, VertexArray, DYNAMIC_DRAW, FLOAT, LINES};
+use glow::{Buffer, HasContext, VertexArray, ARRAY_BUFFER, DYNAMIC_DRAW, FLOAT, LINES};
 use crate::render::Shader;
 
 pub struct LineRenderer {
@@ -24,15 +24,17 @@ pub struct LineRenderer {
  * - float3 pos2
  * - float3 color2
  *
- * Floats: 6
+ * Floats: 6 // Per Vertex + Color
  * Bytes: 48
  */
 const FLOATS: usize = 6;
 const FLOAT_SIZE: usize = size_of::<f32>();
+
 #[cfg(target_arch = "wasm32")]
 const SHADER_VERT: &str = include_str!("../../../resources/shaders/web/line_renderer.vert");
 #[cfg(target_arch = "wasm32")]
 const SHADER_FRAG: &str = include_str!("../../../resources/shaders/web/line_renderer.frag");
+
 #[cfg(not(target_arch = "wasm32"))]
 const SHADER_VERT: &str = include_str!("../../../resources/shaders/native/line_renderer.vert");
 #[cfg(not(target_arch = "wasm32"))]
@@ -41,33 +43,53 @@ const SHADER_FRAG: &str = include_str!("../../../resources/shaders/native/line_r
 impl LineRenderer {
     pub fn new(gl: Rc<glow::Context>, capacity: usize) -> Result<Self, String> {
         unsafe {
+			let vec = Vec::with_capacity(capacity);
             let shader = Shader::newVertFrag(gl.clone(), SHADER_VERT, SHADER_FRAG)?;
+			
+			let vao = gl.create_vertex_array().map_err(|e| format!("Failed to create vertex array: {}", e))?;
+			let vbo = gl.create_buffer().map_err(|e| format!("Failed to create buffer object: {}", e))?;
+			
+			gl.bind_vertex_array(Some(vao));
+			gl.bind_buffer(ARRAY_BUFFER, Some(vbo));
+			gl.buffer_data_u8_slice(ARRAY_BUFFER, bytemuck::cast_slice(&vec), DYNAMIC_DRAW);
+			
+			let locPos = shader.getAttribLocation("i_position").unwrap();
+			let locCol = shader.getAttribLocation("i_color").unwrap();
+			
+			let mut offset: usize = 0;
+			let stride = (6 * FLOAT_SIZE) as i32;
+			gl.vertex_attrib_pointer_f32(locPos, 3, FLOAT, false, stride, offset as i32);
+			gl.enable_vertex_attrib_array(locPos);
+			
+			offset += 3 * FLOAT_SIZE;
+			gl.vertex_attrib_pointer_f32(locCol, 3, FLOAT, false, stride, offset as i32);
+			gl.enable_vertex_attrib_array(locCol);
+			
+			gl.bind_buffer(ARRAY_BUFFER, None);
+			gl.bind_vertex_array(None);
 
-
-
-            let vao = gl.create_vertex_array().map_err(|e| format!("Failed to create vertex array: {}", e))?;
-            let vbo = gl.create_named_buffer().map_err(|e| format!("Failed to create buffer object: {}", e))?;
-            gl.bind_vertex_array(Some(vao));
-
-            // gl.named_buffer_data_size(vbo, (capacity * FLOAT_SIZE) as i32, DYNAMIC_DRAW);
-            let vec = Vec::with_capacity(capacity);
-            gl.named_buffer_data_u8_slice(vbo, bytemuck::cast_slice(&vec), DYNAMIC_DRAW);
-            gl.vertex_array_vertex_buffer(vao, 0, Some(vbo), 0, (FLOATS * FLOAT_SIZE) as i32);
-
-            let locPos = shader.getAttribLocation("i_position").unwrap();
-            let locCol = shader.getAttribLocation("i_color").unwrap();
-
-            let mut offset: usize = 0;
-            gl.vertex_array_attrib_format_f32(vao, locPos, 3, FLOAT, false, offset as u32);
-            gl.vertex_array_attrib_binding_f32(vao, locPos, 0);
-            offset += 3 * FLOAT_SIZE;
-
-            gl.vertex_array_attrib_format_f32(vao, locCol, 3, FLOAT, false, offset as u32);
-            gl.vertex_array_attrib_binding_f32(vao, locCol, 0);
+            // let vao = gl.create_vertex_array().map_err(|e| format!("Failed to create vertex array: {}", e))?;
+            // let vbo = gl.create_named_buffer().map_err(|e| format!("Failed to create buffer object: {}", e))?;
+            // gl.bind_vertex_array(Some(vao));
+			//
+            // // gl.named_buffer_data_size(vbo, (capacity * FLOAT_SIZE) as i32, DYNAMIC_DRAW);
+            // gl.named_buffer_data_u8_slice(vbo, bytemuck::cast_slice(&vec), DYNAMIC_DRAW);
+            // gl.vertex_array_vertex_buffer(vao, 0, Some(vbo), 0, (FLOATS * FLOAT_SIZE) as i32);
+			//
+            // let locPos = shader.getAttribLocation("i_position").unwrap();
+            // let locCol = shader.getAttribLocation("i_color").unwrap();
+			//
+            // let mut offset: usize = 0;
+            // gl.vertex_array_attrib_format_f32(vao, locPos, 3, FLOAT, false, offset as u32);
+            // gl.vertex_array_attrib_binding_f32(vao, locPos, 0);
             // offset += 3 * FLOAT_SIZE;
-
-            gl.enable_vertex_array_attrib(vao, locPos);
-            gl.enable_vertex_array_attrib(vao, locCol);
+			//
+            // gl.vertex_array_attrib_format_f32(vao, locCol, 3, FLOAT, false, offset as u32);
+            // gl.vertex_array_attrib_binding_f32(vao, locCol, 0);
+            // // offset += 3 * FLOAT_SIZE;
+			//
+            // gl.enable_vertex_array_attrib(vao, locPos);
+            // gl.enable_vertex_array_attrib(vao, locCol);
 
             Ok(LineRenderer {
                 gl,
@@ -113,16 +135,25 @@ impl LineRenderer {
         self.shader.setMatrix4f("u_pvm", pvMatrix);
 
         unsafe {
-            if self.floatsPushed > self.lastFloatsPushed {
-                self.gl.named_buffer_data_u8_slice(self.vbo, bytemuck::cast_slice(&self.vec), DYNAMIC_DRAW);
-            } else {
-                self.gl.named_buffer_sub_data_u8_slice(self.vbo, 0, bytemuck::cast_slice(&self.vec));
-            }
+			self.gl.bind_vertex_array(Some(self.vao));
+			self.gl.bind_buffer(ARRAY_BUFFER, Some(self.vbo));
+			
+			self.gl.buffer_data_u8_slice(ARRAY_BUFFER, bytemuck::cast_slice(&self.vec), DYNAMIC_DRAW);
+			
+            // if self.floatsPushed > self.lastFloatsPushed {
+			// 	self.gl.buffer_data_u8_slice(ARRAY_BUFFER, bytemuck::cast_slice(&self.vec), DYNAMIC_DRAW);
+            //     // self.gl.named_buffer_data_u8_slice(self.vbo, bytemuck::cast_slice(&self.vec), DYNAMIC_DRAW);
+            // } else {
+			// 	self.gl.buffer_sub_data_u8_slice(ARRAY_BUFFER, 0, bytemuck::cast_slice(&self.vec));
+            //     // self.gl.named_buffer_sub_data_u8_slice(self.vbo, 0, bytemuck::cast_slice(&self.vec));
+            // }
 
-            self.gl.bind_vertex_array(Some(self.vao));
             let drawCount = self.vec.len() / FLOATS;
             // println!("drawCount: {}", drawCount);
             self.gl.draw_arrays(LINES, 0, drawCount as i32);
+			
+			self.gl.bind_buffer(ARRAY_BUFFER, None);
+			self.gl.bind_vertex_array(None);
         }
 
         self.vec.clear();
